6759a845543896a059d49344291b902826bcd8b0
Modis <cjmodis@gmail.com>
2020-08-26 11:55:31 +0300

    [2.0.6.0] Added processing of stairs

--- mmRebarSolidAndVisible/ObjReinPickFilter.cs
+++ mmRebarSolidAndVisible/ObjReinPickFilter.cs
@@ -17,13 +17,14 @@ namespace mmRebarSolidAndVisible
 
         public static bool IsAllowableElement(Element e)
         {
-            return e.Category.Id.IntegerValue == (int) BuiltInCategory.OST_StructuralColumns ||
-                   e.Category.Id.IntegerValue == (int) BuiltInCategory.OST_StructuralFoundation ||
-                   e.Category.Id.IntegerValue == (int) BuiltInCategory.OST_Floors ||
-                   e.Category.Id.IntegerValue == (int) BuiltInCategory.OST_Walls ||
-                   e.Category.Id.IntegerValue == (int) BuiltInCategory.OST_StructuralFraming ||
-                   e.Category.Id.IntegerValue == (int) BuiltInCategory.OST_Rebar ||
-                   e.Category.Id.IntegerValue == (int) BuiltInCategory.OST_EdgeSlab;
+            return e.Category.Id.IntegerValue == (int)BuiltInCategory.OST_StructuralColumns ||
+                   e.Category.Id.IntegerValue == (int)BuiltInCategory.OST_StructuralFoundation ||
+                   e.Category.Id.IntegerValue == (int)BuiltInCategory.OST_Floors ||
+                   e.Category.Id.IntegerValue == (int)BuiltInCategory.OST_Walls ||
+                   e.Category.Id.IntegerValue == (int)BuiltInCategory.OST_StructuralFraming ||
+                   e.Category.Id.IntegerValue == (int)BuiltInCategory.OST_Rebar ||
+                   e.Category.Id.IntegerValue == (int)BuiltInCategory.OST_EdgeSlab ||
+                   e.Category.Id.IntegerValue == (int)BuiltInCategory.OST_Stairs;
         }
     }
 }
